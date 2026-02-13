dd7400137ac929ce1ddf6796b6b9dc4125ca5bbf
jooweeah <juliainorbit@gmail.com>
2026-02-12 19:34:50 -0500

    Fixed ToDo comments in Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,25 +4,16 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems }) {
   let [newMenuItem, setNewMenuItem] = useState("")
-  // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
-    console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
-
-  // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
-
-  // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
-  // its own item.
+    setMenuItems((prev) => [newMenuItem, ...prev])
+  }, [newMenuItem])
+  
   return (
     <div>
       <input
@@ -34,12 +25,19 @@ export default function Sidebar() {
       <br />
       <button
         onClick={() => {
-          /* TODO: 3 */
+          addMenuItem()
         }}
       >
         Add Item
       </button>
       <br />
+      <ul>
+        {menuItems
+          .filter((item) => new RegExp(filter, "i").test(item))
+          .map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+      </ul>
       <input
         id="filter"
         type="text"
